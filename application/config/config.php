adf4136ae5268cfd8881523c6d6f6c8d98682f87
Khozin <1741723012kh@gmail.com>
2018-06-25 10:07:42 +0700

    remove line

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . str_replace( basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME'] );
-$config['base_url'] = 'http://localhost:8080/onlineshop';
 
 /*
 |--------------------------------------------------------------------------
