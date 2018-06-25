cf1352ab0343962c0753c1dd01632c746d4a1333
Khozin <1741723012kh@gmail.com>
2018-06-25 09:58:21 +0700

    images logo

--- application/views/LanderApp/System/AllData.php
+++ application/views/LanderApp/System/AllData.php
@@ -42,7 +42,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</tr>
 				<tr>
 					<th>Logo Toko</th>
-					<td></td>
+					<td><button type="button" class="btn btn-default btn-rounded faa-parent animated-hover"  data-toggle="modal" data-target="#logoToko" data-toggle="tooltip" data-placement="top" title="" data-original-title="Lihat Gambar berita"><i class="fa fa-picture-o faa-vertical"></i> Lihat Gambar</button></td>
+					<div id="logoToko" class="modal fade" tabindex="-1" role="dialog" style="display: none;">
+						<div class="modal-dialog modal-lg">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+									<h4 class="modal-title text-center"><?=$rows['logo_app']?></h4>
+								</div>
+								<div class="modal-body text-center">
+									<img src="<?=base_url()?>assets/images/<?=$rows['logo_app']?>" width="80%">
+								</div>
+								<div class="modal-footer text-center">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</tr>
 				<tr>
 					<th>Favicon</th>
